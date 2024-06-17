79c0a5119612b5566dc25bd32d5b5c76380ff99e
OliveMatei <smatei@kabarak.ac.ke>
2024-06-17 02:09:32 -0700

    posting

--- app/models/engine/dbStorage.py
+++ app/models/engine/dbStorage.py
@@ -8,7 +8,9 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from app.models.base_model import Base
 from app.models.user import User
 from app.models.company import Company
-classes = {User.__name__: User, Company.__name__: Company}
+from app.models.jobs import Job
+classes = {User.__name__: User, Company.__name__: Company,Job.__name__:Job}
+
 
 
 class DBstorage:

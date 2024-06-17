6c14aa459f23ef29c163a1bcca72d0ed92215f38
Brian Kimathi M <briankimathi94@gmail.com>
2024-06-17 09:30:55 +0300

    posting b

---
+++ app/models/notification.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models.base_model import BaseClass, Base
+
+
+class Notifications(BaseClass, Base):
+    __tablename__ = 'notifications'
+    UserID = Column(Integer, ForeignKey('users.id'))
+    NotificationType = Column(String)
+    Message = Column(String)
+    IsRead = Column(Boolean)
+    CreatedAt = Column(DateTime)
+
+    user = relationship("Users", foreign_keys=[UserID])

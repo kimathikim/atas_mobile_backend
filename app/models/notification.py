16acb90dcb60d627fdb41e2203e20779b84dbb37
Brian Kimathi <90571049+kimathikim@users.noreply.github.com>
2024-06-17 09:40:03 +0300

    Update notification.py

--- app/models/notification.py
+++ app/models/notification.py
@@ -3,10 +3,12 @@ from sqlalchemy.orm import relationship
 from app.models.base_model import Base, BaseClass
 
 class Notifications(Base):
+    
     __tablename__ = 'notifications'
     UserID = Column(Integer, ForeignKey('users.id'))
     NotificationType = Column(String)
     Message = Column(String)
     IsRead = Column(Boolean)
 
+    
     user = relationship("Users")

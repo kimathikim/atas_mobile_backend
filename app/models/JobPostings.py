f91d59f5004e3badd6f54edb827631e1ef7ba6d2
Brian Kimathi M <briankimathi94@gmail.com>
2024-06-17 10:41:31 +0300

    debug job posting

--- app/models/JobPostings.py
+++ app/models/JobPostings.py
@@ -1,7 +1,12 @@
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from app.models.base_model import Base, BaseClass
+# import relationship
+from app.models.User import User
+from app.models.JobPostings import Job
+from sqlalchemy.orm import relationship
 
-class Job(BaseClass,Base):
+
+class Job(BaseClass, Base):
     __tablename__ = 'jobs'
     EmployerID = Column(Integer, ForeignKey('users.UserID'), nullable=False)
     JobTitle = Column(String(255), nullable=False)
@@ -10,8 +15,8 @@ class Job(BaseClass,Base):
     SalaryRange = Column(String(100), nullable=True)
     Location = Column(String(255), nullable=True)
     Requirements = Column(Text, nullable=True)
-    
+
     employer = relationship('User', back_populates='jobs')
 
-    User.jobs = relationship('Job', order_by=Job.JobID, back_populates='employer')
-
+    User.jobs = relationship('Job', order_by=Job.id,
+                             back_populates='employer')
